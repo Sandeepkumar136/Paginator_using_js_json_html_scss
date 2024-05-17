8eda9311b8d6a7b5b8811376afebf7920de822e2
Sandeep_kumar136 <Ksundip750#gmail.com>
2024-05-17 23:04:33 +0530

    initializing js file:

--- script.js
+++ script.js
@@ -9,10 +9,28 @@ function DisMenuItem(Peram){
     return Config;
 }
 
+const card_in_page=4;
+let currentpage=1;
+let totalpage=0;
+let data=[];
+
+
 fetch('./data.json')
 .then(response=>response.json())
 .then(data=>{
-    const container=document.getElementById("card-container");
-    container.innerHTML=DisMenuItem(data);
+    // const container=document.getElementById("card-container");
+    // container.innerHTML=DisMenuItem(data);
+    totalpage=Math.ceil(data.length/card_in_page);
+    displaypage(currentpage)
+    // setupPagingation();
 })
-.catch(error=>console.error('Error fectching the JSON data', error))
+.catch(error=>console.error('Error fectching the JSON data', error));
+
+function displaypage(page){
+    const startIndex=(page -1)*card_in_page;
+    const endIndex=startIndex+card_in_page;
+    const paginatedData=data.slice(startIndex, endIndex);
+    const container=document.getElementById('card-container');
+    container.innerHTML=DisMenuItem(paginatedData)
+}
+
